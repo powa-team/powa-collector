4a1e3d43480c8cb9605557d0b813f221bfccdc21
Julien Rouhaud <julien.rouhaud@free.fr>
2019-03-31 16:20:33 +0200

    Add packaging.

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+__VERSION__ = None
+with open("powa_collector/__init__.py", "r") as fh:
+    for line in fh:
+        if line.startswith('__VERSION__'):
+            __VERSION__ = line.split('=')[1].replace("'", '').strip()
+            break
+
+requires = ['psycopg2']
+
+setuptools.setup(
+    name="powa-collector",
+    version=__VERSION__,
+    author="powa-team",
+    license='Postgresql',
+    author_email="rjuju123@gmail.com",
+    description="PoWA collector, a collector for performing remote snapshot with PoWA",
+    long_description="See https://powa.readthedocs.io/",
+    long_description_content_type="text/markdown",
+    url="https://powa.readthedocs.io/",
+    packages=setuptools.find_packages(),
+    install_requires=requires,
+    classifiers=[
+        "Development Status :: 4 - Beta"
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: PostgreSQL License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: System Administrators",
+        "Topic :: Database :: Front-Ends"
+    ],
+)

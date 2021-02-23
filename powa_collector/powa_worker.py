3ff9cb7a0f592be9eb3b0fe92c7f89cc4914afe6
Marc Cousin <marc.cousin@people-doc.com>
2021-02-23 11:15:51 +0100

    More agressive restart of all connections

--- powa_collector/powa_worker.py
+++ powa_collector/powa_worker.py
@@ -398,13 +398,8 @@ class PowaThread (threading.Thread):
                     self.__take_snapshot()
                 except psycopg2.Error as e:
                     self.logger.error("Error during snapshot: %s" % e)
-                    if (self.__repo_conn is None
-                            or self.__repo_conn.closed > 0):
-                        self.__disconnect_repo()
-                    if (self.__remote_conn is not None
-                            and self.__remote_conn.closed > 0):
-                        self.__remote_conn.close()
-                        self.__remote_conn = None
+                    # It will reconnect automatically at next snapshot
+                    self.__disconnect_all()
 
                 self.last_time = calendar.timegm(time.gmtime())
             time_to_sleep = self.__config["frequency"] - (cur_time -

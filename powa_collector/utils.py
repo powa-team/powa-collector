8220702c2b6f79ac72aee7470f511097da0ef4c7
Julien Rouhaud <julien.rouhaud@free.fr>
2023-06-24 10:51:16 +0800

    Move get_powa_version() to a new powa_collector.utils module.
    A following commit will need to use that function in the main class.
    
    While at it also move conf_are_equal to this module.  It's not strictly
    required but it's cleaner.

---
+++ powa_collector/utils.py
@@ -0,0 +1,32 @@
+"""
+General functions shared between the main thread and the remote server threads.
+"""
+def get_powa_version(conn):
+    """Get powa's extension version"""
+    cur = conn.cursor()
+    cur.execute("""SELECT
+        regexp_split_to_array(extversion, '\\.'),
+        extversion
+        FROM pg_catalog.pg_extension
+        WHERE extname = 'powa'""")
+    res = cur.fetchone()
+    cur.close()
+
+    return res
+
+
+def conf_are_equal(conf1, conf2):
+    """Compare two configurations, returns True if equal"""
+    for k in conf1.keys():
+        if (k not in conf2):
+            return False
+        if (conf1[k] != conf2[k]):
+            return False
+
+    for k in conf2.keys():
+        if (k not in conf1):
+            return False
+        if (conf1[k] != conf2[k]):
+            return False
+
+    return True
